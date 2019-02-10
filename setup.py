34f36377e887fa7ebc3a4a7d5caee6e88c4e5011
yerihyo <yerihyo@gmail.com>
2019-02-10 11:06:51 -0800

    change in library name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 
 setuptools.setup(
-    name="uwo_lib",
-    version="0.1.0",
+    name="unchartedwatersonline-lib",
+    version="0.1.1.1",
     author="yerihyo",
     author_email="yerihyo@gmail.com",
     description="UWO price image reader",
     #long_description=long_description,
     #long_description_content_type="text/markdown",
-    url="https://github.com/foxytrixy-com/uwo_lib",
+    url="https://github.com/foxytrixy-com/unchartedwatersonline-lib",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
